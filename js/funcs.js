718d0cfa042fff434fe025734af92de9222182de
carlos <carlos@cppDWES.es>
2024-08-15 02:20:02 +0200

    vf

--- js/funcs.js
+++ js/funcs.js
@@ -1,19 +1,66 @@
 
-function oculta(){
-    $('#respuesta').html("");
-    $('#youQr').attr("hidden","");
-    $('#cleanQR').attr("hidden","");
-  
-}
+//Download function
 
-function enviar (){
-    var direc = $("#direccion").val();
-    var d = direc.trim();
+$(window).on( "load", function(){
+    $("#download").on( "click", function(){
+        const downloadInstance = document.createElement('a');
+        var imgSrc = $('#imgQr').attr('src');
+
+        downloadInstance.href = imgSrc;
+        downloadInstance.target = '_blank';
+        downloadInstance.download = 'MyQR';
+        downloadInstance.id = "adwl";
+            
+        document.body.appendChild(downloadInstance);
+        downloadInstance.click();
+        document.body.removeChild(downloadInstance);
+    });
+});
+
+
+//Set size values function
+
+$(window).on( "load", function() {
+    $('#high').on("input", function(){
+        var val = $(this).val();
+         $('#highVal').val(val) = $('#high').val();
+    });
+    $('#width').on("input", function(){
+        var valu = $(this).val();
+         $('#widthVal').val(valu) = $('#width').val();
+    });  
+  });
+
+  //Hidde ajax response
+
+$(window).on( "load", function(){
+    $('#cleanQR').on( "click", function(){
+        $('#respuesta').html("");
+        $('#youQr').attr("hidden","");
+        $('#cleanQR').attr("hidden","");
+        $('#download').attr("hidden","");
+    });
+});
+
+//Send Ajax request
+
+$(window).on( "load", function(){
+    $("#Enviar").on( "click", function(){
+        var direc = $("#direccion").val();
+        var h = $("#high").val();
+        var w = $("#width").val();
+        var d = direc.trim();
+
     if((d == "")){
         alert("Please, insert a valid URL");
     }else{
         $.ajax({
-            type: "POST", url: "../src/generator.php", data: "url="+d,
+            type: "POST", url: "../src/generator.php", 
+            data: {
+                url: d, 
+                highV: h, 
+                widthV: w
+            },
             statusCode: {
                 404: function() {alert("Page not found");}
             },
@@ -21,8 +68,10 @@ function enviar (){
                 $('#respuesta').html(result);
                 $('#youQr').removeAttr("hidden");
                 $('#cleanQR').removeAttr("hidden");
+                $('#download').removeAttr("hidden");
             }
         });
         return false;
     }
-}
+    });
+});
--- src/generator.php
+++ src/generator.php
@@ -1,23 +1,42 @@
 <?php
+session_start();
 require "../vendor/autoload.php";
 
 use \Milon\Barcode\DNS2D;
 
+function download(){
+    global $resul2;
+    $rutaImg = "data:image/png;base64" . $resul2;
+    $content = file_get_contents($rutaImg);
+    header('Content-disposition: attachment; filename='.$rutaImg);
+    header('Content-type: image/png');
+    readfile($rutaImg);
+}
+
 if(!isset($_POST['url'])){
-    header('Location: ../public/index.html');
+    header('Location: ../public/index.php');
 }
 
 if(isset($_POST['url'])){
-    $res = trim($_POST['url']);
+    $res = trim($_POST['url']); //Url 
+    $width = $_POST['widthV']; //Ancho
+    $high = $_POST['highV']; //Alto
+    $widthNumber = intval($width);
+    $highNumber = intval($high);
+ 
+}
+
+if(isset($_POST['dwlb'])){
+
 }
     $d = new DNS2D();
     $d->setStorPath(__DIR__.'/cache/');
     try{
         //$result = $d->getBarcodeHTML($res, 'QRCODE',8,8);
-        $resul2 = $d->getBarcodePNG($res,'QRCODE',10,10);
+        $resul2 = $d->getBarcodePNG($res,'QRCODE',$widthNumber,$highNumber);
     }catch(ErrorException $e){
         die($e->getMessage());
     }
-    echo "<img src='data:image/png;base64," . $resul2 . "'/>";
+    echo "<img id='imgQr' src='data:image/png;base64," . $resul2 . "'/>";
     
   
